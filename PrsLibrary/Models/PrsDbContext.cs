4f01b45f35ba2010f632c52db541d21655b869cf
Greg Doud <gdoud@gmail.com>
2022-02-14 12:06:10 -0500

    Add project files.

---
+++ PrsLibrary/Models/PrsDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrsLibrary.Models {
+
+    public class PrsDbContext : DbContext {
+
+        public virtual DbSet<User> Users { get; set; }
+
+        public PrsDbContext() { }
+
+        public PrsDbContext(DbContextOptions<PrsDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder) {
+            if(!builder.IsConfigured) {
+                builder.UseSqlServer(
+                    "server=localhost\\sqlexpress;database=TestPrsDb;trusted_connection=true;"
+                );
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            // makes Username in User unique
+            builder.Entity<User>(e => {
+                e.HasIndex(p => p.Username).IsUnique(true);
+            });
+        }
+    }
+}
